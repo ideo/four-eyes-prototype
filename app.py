44d37125a827cc25fe776ed2f59c2304a71208de
Joe Gambino <joe@gam.bingo>
2024-02-28 14:06:05 -0600

    better prompt

--- app.py
+++ app.py
@@ -9,27 +9,19 @@ from app import image_processing as image
 from app.horoscope import horoscope
 
 
+st.set_page_config(
+    page_title="HoroLens",
+    page_icon="👓"
+)
+
 lg.initialize_session_state()
 
 
-st.title("HoroLens")
+st.title("AstroLens")
 picture = st.camera_input("Take a Picture")
-# picture = Image.open("data/IMG_8769.JPG")
 
 if picture:
     text = image.extract_text_from_image(picture)
-
-    # text = [
-    #     "Cigna Health and Life Insurance Co. Coverage Effective Date: 01/01/2024",
-    #     "Group: 3343029",
-    #     "ID: U80060737 01 Name: Joe Gambino",
-    #     "5 Jesse White + Secretary of FeseatLinieapply aduc no:6515-4829-1 22¢ 3 pes:08/03/1994 & weer 08/03/2025. |",
-    #     "pos:08/03/1991 «exe 08/03/20",
-    #     "wducno:6515-48 29-4 neo = % vos:08/03/1994 av exe: 08/03/2025.",
-    # ]
-
-    # st.write(text)
-
     dates = image.recognize_dates(text)
     birthday = image.extract_likely_birthday(dates)
 
@@ -41,4 +33,4 @@ if picture:
 
     else:
         picture = None
-        st.success("Oh no! I can't find your birthday. Please take a new picture.")
+        st.warning("Oh no! I can't find your birthday. Please take a new picture.")
